8da30568a6c02c72f79708ed4c06870c5f47614b
eden sharp <edenasharpie@gmail.com>
2026-02-04 20:48:01 -0500

    weird inconsistency

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -2,7 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("dashboard", "routes/dashboard.tsx"),
   /* demo purposes only */
   route("components-demo", "routes/components-demo.tsx")
 ] satisfies RouteConfig;
