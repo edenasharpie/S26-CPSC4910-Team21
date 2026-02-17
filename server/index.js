12baed4bd4c1aed6b5af5cc3928b3ebbeb783b5a
eden sharp <edenasharpie@gmail.com>
2026-02-17 13:41:19 -0500

    added environment setup & access

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config({ path: '../../.fs-env' });
 
 // API endpoints
 const catalogRoutes = require('./src/routes/catalogs');
